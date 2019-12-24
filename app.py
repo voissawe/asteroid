7203ef3e2b54c60f19163ed5e5c5d4a9979d6a26
Yusuf Cihan <54748183+ysfchn@users.noreply.github.com>
2019-12-24 09:11:26 +0300

    AsteroidDB v1.1 (#3)
    * Bug Fix - UnboundLocalError
    
    * Feature - /version method

--- app.py
+++ app.py
@@ -188,14 +188,14 @@ def set_key():
             if getpassword.value == oldpassword:
                 getpassword.value = newpassword
                 db.session.commit()
-                return jsonify(action="CHANGED PASSWORD", oldpassword=oldpassword, newpassword=newpassword)
+                return jsonify(action="CHANGED PASSWORD", oldpass=oldpassword, newpass=newpassword)
             else:
                 return return_error('Wrong old password!')
         else:
             data = TinyWebDB(tag='dbpass', value=newpassword)
             db.session.add(data)
             db.session.commit()
-            return jsonify(action="SET PASSWORD", oldpassword=oldpassword, newpassword=newpassword)
+            return jsonify(action="SET PASSWORD", oldpass=oldpassword, newpass=newpassword)
     return return_error('No new password is specified!')
 
 
@@ -247,6 +247,14 @@ def count_all():
     	resl = resl - 1
     return jsonify(action="COUNT", count=resl)
 
+
+# -------------------------
+#  Version
+#  - Shows the tagged version of AsteroidDB instance. 
+# -------------------------
+@app.route('/version')
+def version():
+    return jsonify(action="VERSION", result="1.1")
 
 # -------------------------
 #  Is Locked?
